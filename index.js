149809194829c241f16781764dcc57d93b57ade8
Caden Lalley <cadenlalley@gmail.com>
2024-06-24 16:58:52 -0400

    Taking a break at lesson #8

--- index.js
+++ index.js
@@ -1,13 +1,60 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 
+// Import database
+import db from './_db.js';
+
 // Import types
-import { typeDefs } from './typeDefs';
+import { typeDefs } from './schema.js';
+
+const resolvers = {
+    Query: {
+        games() {
+            return db.games
+        },
+        game(_, args) {
+            return db.games.find(game => game.id === args.id); // When the inner logic resolves to true, we will retrieve that object and send it to the client.
+        },
+        reviews() {
+            return db.reviews
+        },
+        review(_, args) {
+            return db.reviews.find(review => review.id === args.id); // When the inner logic resolves to true, we will retrieve that object and send it to the client.
+        },
+        authors() {
+            return db.authors
+        },
+        author(_, args) {
+            return db.authors.find(author => author.id === args.id); // When the inner logic resolves to true, we will retrieve that object and send it to the client.
+        }
+    },
+
+    Game: { // Given a single game object - using the single game query with id param...
+        reviews(parent) { // Find the reviews associated with that game.
+            return db.reviews.filter(review => review.game_id === parent.id); // Return all reviews associated with the current game.
+        }
+    },
+
+    Author: {
+        reviews(parent) {
+            return db.reviews.filter(review => review.author_id === parent.id);
+        }
+    },
+
+    Review: {
+        author(parent) {
+            return db.authors.find(author => author.id === parent.author_id);
+        },
+        game(parent) {
+            return db.games.find(game => game.id === parent.game_id);
+        }
+    }
+}
 
 // Server Setup
 const server = new ApolloServer({
     typeDefs,
-    // typeDefs and resolvers...
+    resolvers
 });
 
 const { url } = await startStandaloneServer(server, {
--- schema.js
+++ schema.js
@@ -3,24 +3,31 @@ export const typeDefs = `#graphql
         id: ID! # Name of field, type of field. 
         title: String! # ! means it's required.
         platform: [String!]! # This means that there must be an array, and strings within the array. 
+        reviews: [Review!] # Relationship between different data.
     }    
 
     type Review {
         id: ID!
         rating: Int!
         content: String!
+        game: Game! # Relationship between different data.
+        author: Author! # Relationship between different data.
     }
 
     type Author {
         id: ID!
         name: String!
         verified: Boolean!
+        reviews: [Review!] # Relatiosnip between different data.
     }
 
     type Query {
         reviews: [Review]
+        review(id: ID): Review # In order to query based on a field, you must delcare a graph entry point that defines a query variable and the enrty point into the graph.
         games: [Game]
+        game(id: ID!): Game
         authors: [Author]
+        author(id: ID): Author
     }
 
 `
